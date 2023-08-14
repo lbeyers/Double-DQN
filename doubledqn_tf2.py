bef129a9a7b87e431bcccaeabec7b6abb580986a
lbeyers <louise@beyers.co.za>
2023-08-14 15:02:26 +0200

    Trying to add the actual files

---
+++ doubledqn_tf2.py
@@ -0,0 +1,140 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import sonnet as snt
+from tensorflow.keras.saving import load_model
+
+class ReplayBuffer():
+	def __init__(self, max_size, input_dims):
+		self.mem_size = max_size
+		self.mem_cntr = 0
+		
+		self.state_memory = np.zeros((self.mem_size,*input_dims), dtype=np.float32)
+		self.new_state_memory = np.zeros((self.mem_size, *input_dims), 
+					dtype = np.float32)
+		self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+		self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+		self.terminal_memory = np.zeros(self.mem_size, dtype = np.int32)
+		
+	def store_transition(self,state,action,reward,state_, done):
+		index = self.mem_cntr % self.mem_size
+		self.state_memory[index] = state
+		self.new_state_memory[index] = state_
+		self.reward_memory[index] = reward
+		self.action_memory[index] = action
+		self.terminal_memory[index] = 1 - int(done)
+		self.mem_cntr += 1
+		
+	def sample_buffer(self, batch_size):
+		max_mem = min(self.mem_cntr, self.mem_size)
+		batch = np.random.choice(max_mem, batch_size, replace = False)
+		
+		states = self.state_memory[batch]
+		states_ = self.new_state_memory[batch]
+		rewards = self.reward_memory[batch]
+		actions = self.action_memory[batch]
+		terminal = self.terminal_memory[batch]
+		
+		return states, actions, rewards, states_, terminal
+		
+def build_dqn(n_actions, fc1_dims, fc2_dims):
+	model = snt.Sequential([
+		snt.Linear(fc1_dims),
+		tf.nn.relu,
+		snt.Linear(fc2_dims),
+		tf.nn.relu,
+		snt.Linear(n_actions)
+	])
+	return model
+		
+class Agent():
+	def __init__(self, lr, gamma, n_actions, epsilon, batch_size,
+			input_dims, epsilon_dec=1e-4, epsilon_end=0.01,
+			mem_size=1e6, fname='doubledqn_model.h5'):
+		self.action_space = [i for i in range(n_actions)]
+		self.gamma = gamma
+		self.epsilon = epsilon
+		self.eps_dec = epsilon_dec
+		self.eps_min = epsilon_end
+		self.batch_size = batch_size
+		self.model_file = fname
+		self.memory = ReplayBuffer(mem_size, input_dims)
+		self.q_online = build_dqn(n_actions,256,256)
+		self.q_target = build_dqn(n_actions,256,256)
+		self.optimizer = snt.optimizers.RMSProp(learning_rate=lr)
+
+	# update target network every now and again
+	def update_target_network(self):
+		# https://github.com/deepmind/sonnet/issues?page=1&q=is%3Aissue+is%3Aopen
+		tf.saved_model.save(self.q_online,self.model_file)
+		self.q_target = tf.saved_model.load(self.model_file)
+
+		# online_variables = self.q_online.variables
+		# target_variables = self.q_target.variables
+		# for source, dest in zip(online_variables, target_variables):
+		# 	dest.assign(source)
+		
+	def store_transition(self, state, action, reward, new_state, done):
+		self.memory.store_transition(state, action, reward, new_state, done)
+		
+	def choose_action(self, observation):
+		if np.random.random() < self.epsilon:
+			action = np.random.choice(self.action_space)
+		else:
+			# https://stackoverflow.com/questions/64520917/valueerror-error-when-checking-input-expected-dense-1-input-to-have-shape-8
+			state = np.reshape(np.array(observation), (1,len(observation)))
+			actions = self.q_online.__call__(state)
+			
+			action = np.argmax(actions)
+			
+		return action
+		
+	def learn(self):
+		if self.memory.mem_cntr < self.batch_size:
+			return
+		states, actions, rewards, states_, dones = \
+			self.memory.sample_buffer(self.batch_size)
+			
+		self.train_step(states, actions, rewards, states_, dones)
+	
+		self.epsilon = self.epsilon - self.eps_dec if self.epsilon > \
+			self.eps_min else self.eps_min
+
+	@tf.function
+	def train_step(self, states, actions, rewards, states_, dones):
+		dones = tf.cast(dones, dtype="float32")
+
+		# for getting the values
+		q_target_next = self.q_target(states_)
+		# for ranking the actions
+		q_online_next = self.q_online(states_)
+		
+		# choose action using online values
+		target_actions = tf.math.argmax(q_online_next, axis=1)
+
+		# grab static values for winning actions
+		q_target = tf.gather(q_target_next,target_actions,batch_dims=1)
+
+		target = rewards + self.gamma * (1 - dones) * q_target
+		# prevents GradientTape from watching these later
+		target = tf.stop_gradient(target)
+
+		with tf.GradientTape() as tape:
+
+			q_online = self.q_online(states)
+			q_taken = tf.gather(q_online,actions,batch_dims=1)
+
+			# mse
+			error = (target-q_taken)**2
+			loss = tf.reduce_mean(error)
+
+		# sonnet
+		variables = self.q_online.trainable_variables
+		gradients = tape.gradient(loss, variables)
+		self.optimizer.apply(gradients, variables)
+			
+	def save_model(self):
+		self.q_online.save(self.model_file)
+	
+	def load_model(self):
+		self.q_online = load_model(self.model_file)
---
+++ doublerun.py
@@ -0,0 +1,90 @@
+from doubledqn_tf2 import Agent
+import numpy as np
+import gymnasium as gym
+import tensorflow as tf
+import wandb
+from absl import flags, app
+import random
+
+FLAGS = flags.FLAGS
+flags.DEFINE_float("lr", 0.01, "Learning Rate")
+flags.DEFINE_float("eps_dec", 0.01, "Decay rate of epsilon")
+flags.DEFINE_integer("seed", 42, "Random seed")
+flags.DEFINE_float("gamma", 0.99, "Gamma value for update")
+
+
+def main(_):
+    env = gym.make('LunarLander-v2')
+    eps_end = 0.01
+    random.seed(FLAGS.seed)
+    np.random.seed(FLAGS.seed)
+
+    doublerun = wandb.init(reinit=True)
+
+    agent = Agent(gamma=FLAGS.gamma, epsilon=1.0,lr=FLAGS.lr, \
+            input_dims=env.observation_space.shape, \
+            n_actions=env.action_space.n,mem_size=100000,batch_size=64, \
+            epsilon_end=eps_end, epsilon_dec=FLAGS.eps_dec)
+    scores = []
+    eps_history = []
+    timesteps = 0
+    max_timesteps = 500000
+    i = 0   #number of episodes
+    tardy = False
+    target_update_period = 500
+
+    while not tardy:
+        done=False
+        score=0
+        episode_seed = random.randint(0,1000)
+        observation, info = env.reset(seed=episode_seed)
+
+        game_length = 0
+        procrastinating = False
+        
+        # to store, convert to unique number
+        # obs_for_storage = hash(tuple(observation[0].flatten()))
+        while not (done or tardy or procrastinating):
+            action = agent.choose_action(observation)
+            
+            observation_, reward, done, truncated, info = env.step(action)
+            score += reward
+            #obs__for_storage = hash(tuple(observation_[0].flatten()))
+            agent.store_transition(observation, action, reward, \
+                observation_, done)
+            observation = observation_
+            # obs_for_storage = obs__for_storage
+            agent.learn()
+
+            timesteps+=1
+            if timesteps>=max_timesteps:
+                tardy=True
+
+            # the lunar lander tries to stay up high - wasted computation
+            game_length +=1
+            if game_length >= 1000:
+                procrastinating = True
+
+            # update target network every n steps
+            if timesteps % target_update_period ==0:
+                agent.update_target_network()
+            
+        eps_history.append(agent.epsilon)
+        scores.append(score)
+        
+        avg_score = np.mean(scores[-100:])
+
+        logs = {'game_length': game_length, 'episode': i ,'score' : score,
+            'average_score' : avg_score,
+            'steps' : timesteps,
+            'epsilon' : agent.epsilon}
+        wandb.log(logs)
+        print(i)
+        i+=1
+
+
+if __name__ == "__main__":
+    app.run(main)
+	
+
+
