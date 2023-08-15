665a4e90b3c058db8eb745560e792e7b1285f5ed
lbeyers <louise@beyers.co.za>
2023-08-15 09:01:52 +0200

    Added SMAC files

---
+++ smac_doublerun.py
@@ -0,0 +1,102 @@
+from smac_doubledqn_tf2 import Agent
+from wrapper_2 import GymWrapper
+import numpy as np
+# import gymnasium as gym
+from smac.env import StarCraft2Env
+import tensorflow as tf
+import wandb
+from absl import flags, app
+import random
+
+FLAGS = flags.FLAGS
+flags.DEFINE_float("lr", 0.001, "Learning Rate")
+flags.DEFINE_float("eps_dec", 0.0001, "Decay rate of epsilon")
+flags.DEFINE_integer("seed", 42, "Random seed")
+flags.DEFINE_float("gamma", 0.99, "Gamma value for update")
+flags.DEFINE_integer("targ_update", 500, "Number of steps before copying network weights")
+flags.DEFINE_integer("buffer_size",100000,"Size of memory")
+
+
+def main(_):
+    env = GymWrapper("3m", FLAGS.seed)
+    eps_end = 0.01
+    random.seed(FLAGS.seed)
+    np.random.seed(FLAGS.seed)
+
+    env.wrap_up()
+
+    doublerun = wandb.init(reinit=True)
+
+    agent = Agent(gamma=FLAGS.gamma, epsilon=1.0,lr=FLAGS.lr, \
+            input_dims=env.obs_len, \
+            n_actions=env.n_actions**env.n_agents,mem_size=FLAGS.buffer_size,batch_size=64, \
+            epsilon_end=eps_end, epsilon_dec=FLAGS.eps_dec)
+    
+    scores = []
+    eps_history = []
+    timesteps = 0
+    max_timesteps = 100000
+    i = 0   #number of episodes
+    tardy = False
+    target_update_period = FLAGS.targ_update
+
+    while not tardy:
+        done=False
+        score=0
+        episode_seed = random.randint(0,1000)
+        #seed set at init
+        env = GymWrapper("3m", seed=episode_seed)
+        observation, available_actions = env.reset()
+
+        game_length = 0
+        procrastinating = False
+        
+        # to store, convert to unique number
+        # obs_for_storage = hash(tuple(observation[0].flatten()))
+        while not (done or tardy or procrastinating):
+            
+            action = agent.choose_action(observation,available_actions)
+            
+            observation_, reward, done, truncated, available_actions = env.step(action)
+            score += reward
+            #obs__for_storage = hash(tuple(observation_[0].flatten()))
+            agent.store_transition(observation, action, reward, \
+                observation_, done)
+            observation = observation_
+            # obs_for_storage = obs__for_storage
+            agent.learn()
+
+            timesteps+=1
+            if timesteps>=max_timesteps:
+                tardy=True
+
+            # the lunar lander tries to stay up high - wasted computation
+            game_length +=1
+            #if game_length >= 1000:
+                #procrastinating = True
+
+            # update target network every n steps
+            if timesteps % target_update_period ==0:
+                agent.update_target_network()
+            
+        eps_history.append(agent.epsilon)
+        scores.append(score)
+        
+        avg_score = np.mean(scores[-100:])
+
+        logs = {'game_length': game_length, 'episode': i ,'score' : score,
+            'average_score' : avg_score,
+            'steps' : timesteps,
+            'epsilon' : agent.epsilon}
+        wandb.log(logs)
+        print(i)
+        i+=1
+
+        env.wrap_up()
+
+
+if __name__ == "__main__":
+    app.run(main)
+	
+
+
---
+++ wrapper_2.py
@@ -0,0 +1,117 @@
+# https://semaphoreci.com/community/tutorials/building-and-testing-an-api-wrapper-in-python
+
+# for an env wrapper, we need:
+# - n_actions: int
+# - the ability to: step
+#   - obs, reward, done
+# - the ability to: reset env
+#   - obs?
+# - the shape of the observations
+import itertools
+import numpy as np
+from smac.env import StarCraft2Env
+#def 
+
+# SMAC 3m
+
+class GymWrapper():
+    def __init__(self, mapname, seed):
+        self.smac_env = StarCraft2Env(map_name=mapname, seed=seed)
+        self.n_actions = self.smac_env.get_env_info()["n_actions"]
+        self.n_agents = self.smac_env.get_env_info()["n_agents"]
+
+        # get length of a flattened observation & hope it stays
+        # the same for a game
+        self.smac_env.reset()
+        obs_list = self.smac_env.get_obs()
+        super_obs = np.array(obs_list).flatten()
+        self.obs_len = super_obs.shape
+
+        # create all combinations with cartesian product
+        self.superact_arr = list(itertools.product(range(self.n_actions),repeat = self.n_agents))
+
+
+    def get_legal_multiact(self):
+
+        avail_acts = []
+        for agent_id in range(self.n_agents):
+            avail_actions = self.smac_env.get_avail_agent_actions(agent_id)
+            avail_acts.append(avail_actions)
+     
+
+        #claude code
+        superact_legls = []
+        for superact in self.superact_arr:
+            legals_sum = 0
+            for i in range(self.n_agents):
+                legals_sum += avail_acts[i][superact[i]]
+            if legals_sum != self.n_agents:
+                superact_legls.append(0) # illegal action
+            else:
+                superact_legls.append(1) # legal action
+        
+        return superact_legls
+    
+    def wrap_up(self):
+        self.smac_env.close()
+
+    def step(self, action):
+        # central writes down orders from tuple
+        action_arr = self.superact_arr[action]
+        reward, terminated, _ = self.smac_env.step(list(action_arr))
+
+        obs_list = self.smac_env.get_obs()
+        super_obs = np.array(obs_list).flatten()
+
+        placeholder=False
+
+        available_actions = self.get_legal_multiact()
+
+        return super_obs, reward, terminated, placeholder, available_actions
+
+    def reset(self):
+        #self.smac_env.seed(seed)
+        self.smac_env.reset()
+
+        obs_list = self.smac_env.get_obs()
+        super_obs = np.array(obs_list).flatten()
+
+        available_actions = self.get_legal_multiact()
+
+        return super_obs, available_actions
+
+    def action_to_multiact(self,action):
+        multiact = []
+        for agent_id in range(self.n_agents):
+            action
+        return multiact
+
+
+## ACTIONS
+# agent1 = [0,1]
+# agent2 = [0,1]
+# agent3 = [0,1]
+
+# for agent in agents:
+
+# SuperAgent = [00, 01, 10, 11]
+
+# SuperAgent = [0,1,2,3]
+
+# 0 -> 00
+# 1 -> 01
+
+
+# OBSERVATIONS
+
+# agent1 [0,0,0]
+# agent2 [0,0,0]
+
+# superObs = [0,0,0,0,0,0]
+
+## LEGALS
+#agent1 = [0,1,2]
+#agent_legals = [1,1,0]
+
+#agent2 = [0,1,2]
+#agent_legals = [0,1,1]
