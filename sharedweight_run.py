fec55a64625cc1917ab659a7254841f88ac6aded
lbeyers <louise@beyers.co.za>
2023-08-17 10:45:20 +0200

    Shared weight implementation

---
+++ sharedweight_run.py
@@ -0,0 +1,140 @@
+from sharedweight_system_dqn import Cohort
+import numpy as np
+import tensorflow as tfs
+import wandb
+from absl import flags, app
+import random
+from smac.env import StarCraft2Env
+
+FLAGS = flags.FLAGS
+flags.DEFINE_float("lr", 3e-4, "Learning Rate")
+flags.DEFINE_float("eps_dec", 1e-5, "Decay rate of epsilon")
+flags.DEFINE_integer("seed", 42, "Random seed")
+flags.DEFINE_float("gamma", 0.99, "Gamma value for update")
+flags.DEFINE_integer("targ_update", 500, "Number of steps before copying network weights")
+flags.DEFINE_integer("buffer_size",200000,"Size of memory")
+
+
+def main(_):
+
+    # hyperparams & fixed variables
+    eps_end = 0.05
+    max_timesteps = 1000000
+    target_update_period = FLAGS.targ_update
+
+    # start the wandb logger
+    run = wandb.init(reinit=True,project="VDN")
+    
+    random.seed(FLAGS.seed)
+    np.random.seed(FLAGS.seed)
+
+    # make StarCraft environment
+    env = StarCraft2Env(map_name="3m", seed=FLAGS.seed)
+
+    # get number of agents and actions in the env
+    env_info = env.get_env_info()
+    n_agents = env_info["n_agents"]
+    n_actions = env_info["n_actions"]   # per agent
+    obs_shape = env_info["obs_shape"]   # per agent
+
+    agent = Cohort(gamma=FLAGS.gamma, epsilon=1.0,lr=FLAGS.lr, \
+        input_dims=[obs_shape], n_agents=n_agents, \
+        n_actions=n_actions,mem_size=FLAGS.buffer_size,batch_size=32, \
+        epsilon_end=eps_end, epsilon_dec=FLAGS.eps_dec)
+
+    # storage & tracking variables - these change in the loop
+    scores = []
+    eps_history = []
+    wins = []
+    timesteps = 0
+    i = 0 
+    tardy = False
+
+    while not tardy:
+        # storage & tracking variables - these change in the loop
+        done=False
+        score=0
+        game_length = 0
+        won = 0
+
+        # todo
+        env.reset()
+        obs_list = env.get_obs()
+        
+        while not (done or tardy):
+            timesteps+=1
+            actions = []
+            for agent_id in range(n_agents):
+                avail_actions = env.get_avail_agent_actions(agent_id)
+                action = agent.choose_action(obs_list[agent_id],avail_actions,agent_id)
+                actions.append(action)
+            
+            # reward and terminated are shared values
+            reward, terminated, info = env.step(actions)
+            try:
+                won = int(info['battle_won'])
+            except:
+                won = 1
+
+            obs_list_ = env.get_obs()
+            score += reward
+            done = terminated 
+
+            # store all transitions and learn
+
+            avail_acts_ls = np.zeros((n_agents,n_actions))
+            for agent_id in range(n_agents):
+                avail_actions = env.get_avail_agent_actions(agent_id)   #already one-hot
+                avail_acts_ls[agent_id] = avail_actions
+
+
+
+            agent.store_transition(np.array(obs_list), actions, reward, \
+            np.array(obs_list_), done, avail_acts_ls)
+
+            train_logs = agent.learn()
+
+            logs = {
+                    'epsilon' : agent.epsilon,
+                    **train_logs
+                }
+
+            if timesteps % target_update_period ==0:
+                agent.update_target_network()
+            logs["steps"] = timesteps
+            wandb.log(logs)
+
+            obs_list = obs_list_
+
+            
+            if timesteps>=max_timesteps:
+                tardy=True
+
+            game_length +=1
+            
+            
+        eps_history.append(agent.epsilon)
+        scores.append(score)
+        wins.append(won)
+        
+        avg_score = np.mean(scores[-100:])
+
+        logs = {'game_length': game_length,
+            'episode': i ,
+            'score' : score,
+            'won' : won,
+            'average_score' : avg_score,
+            'steps' : timesteps,
+            'epsilon' : agent.epsilon}
+        wandb.log(logs)
+
+        i+=1
+        if i % 25 == 0:
+            print(f"episode {i}")
+
+
+if __name__ == "__main__":
+    app.run(main)
+	
+
+
---
+++ sharedweight_system_dqn.py
@@ -0,0 +1,168 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import sonnet as snt
+
+class ReplayBuffer():
+	def __init__(self, max_size, input_dims,action_space_size, n_agents):
+		self.mem_size = max_size
+		self.n_agents = n_agents
+		self.action_space_size = action_space_size
+		self.mem_cntr = 0
+		self.state_memory = np.zeros((self.mem_size,n_agents,*input_dims), dtype=np.float32)
+		self.new_state_memory = np.zeros((self.mem_size, n_agents, *input_dims), 
+					dtype = np.float32)
+		self.action_memory = np.zeros((self.mem_size,n_agents), dtype=np.int32)
+		self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
+		self.terminal_memory = np.zeros(self.mem_size, dtype = np.int32)
+		self.avail_action_memory = np.zeros((self.mem_size,n_agents,action_space_size),dtype=bool)
+		
+	def store_transition(self,state,action,reward,state_, done, avail_act_oh):
+		index = self.mem_cntr % self.mem_size
+		self.state_memory[index] = state
+		self.new_state_memory[index] = state_
+		self.reward_memory[index] = reward
+		self.action_memory[index] = action
+		self.terminal_memory[index] = int(done)
+		self.avail_action_memory[index] = avail_act_oh
+
+		self.mem_cntr += 1
+		
+	def sample_buffer(self, batch_size):
+		max_mem = min(self.mem_cntr, self.mem_size)
+		batch = np.random.choice(max_mem, batch_size, replace = False)
+
+		states = self.state_memory[batch]
+		states_ = self.new_state_memory[batch]
+		rewards = self.reward_memory[batch]
+		actions = self.action_memory[batch]
+		terminal = self.terminal_memory[batch]
+		avail_acts_oh = self.avail_action_memory[batch]
+		
+		return states, actions, rewards, states_, terminal, avail_acts_oh
+		
+# todo change for RNN later?
+def build_dqn(n_actions, fc1_dims, fc2_dims):
+	model = snt.Sequential([
+		snt.Linear(fc1_dims),
+		tf.nn.relu,
+		snt.Linear(fc2_dims),
+		tf.nn.relu,
+		snt.Linear(n_actions)
+	])
+	return model
+		
+class Cohort():
+	def __init__(self, lr, gamma, n_actions, epsilon, batch_size,
+			input_dims, n_agents, epsilon_dec=1e-4, epsilon_end=0.01,
+			mem_size=1e6, fname='smac_doubledqn_model.h5'):
+		self.action_space = [i for i in range(n_actions)]
+		self.gamma = gamma
+		self.epsilon = epsilon
+		self.eps_dec = epsilon_dec
+		self.eps_min = epsilon_end
+		self.batch_size = batch_size
+		self.n_agents = n_agents
+		self.model_file = fname
+		self.memory = ReplayBuffer(mem_size, input_dims, n_actions, n_agents)
+		self.q_online = build_dqn(n_actions,256,256)
+		self.q_target = build_dqn(n_actions,256,256)
+		self.optimizer = snt.optimizers.RMSProp(learning_rate=lr)
+
+	# update target network every now and again
+	def update_target_network(self):
+		online_variables = self.q_online.variables
+		target_variables = self.q_target.variables
+		for source, dest in zip(online_variables, target_variables):
+			dest.assign(source)
+		
+	def store_transition(self, states, actions, reward, new_states, done, avail_acts_ls):
+		self.memory.store_transition(states, actions, reward, new_states, done, avail_acts_ls)
+
+	#def state_concat_agentid(states_block):
+
+		#return state_agent_id
+		
+	def choose_action(self, observation,avail_acts_oh, agent_id):
+		avail_acts = np.nonzero(avail_acts_oh)[0]
+		if np.random.random() < self.epsilon:
+			action = np.random.choice(avail_acts)
+		else:
+			# https://stackoverflow.com/questions/64520917/valueerror-error-when-checking-input-expected-dense-1-input-to-have-shape-8
+			state = np.reshape(np.array(observation), (1,len(observation)))
+
+			# concat with one-hot vector to condition Q-network
+			oh_agentid = np.zeros(self.n_agents,dtype=np.float32)
+			oh_agentid[agent_id] = 1
+			oh_agentid = np.reshape(oh_agentid,(1,self.n_agents))
+			agentid_state = np.concatenate((oh_agentid,state),axis=1,dtype=np.float32)
+
+			actions = self.q_online.__call__(agentid_state).numpy()[0]
+			
+			subset_idx = np.argmax(actions[avail_acts])
+			action = avail_acts[subset_idx]
+			
+		return action
+		
+	def learn(self):
+		if self.memory.mem_cntr < self.batch_size:
+			return {}
+		states, actions, rewards, states_, dones, avail_acts_oh = \
+			self.memory.sample_buffer(self.batch_size)
+			
+		logs = self.train_step(states, actions, rewards, states_, dones, avail_acts_oh)
+	
+		self.epsilon = self.epsilon - self.eps_dec if self.epsilon > \
+			self.eps_min else self.eps_min
+		
+		return logs
+
+	@tf.function
+	def train_step(self, states_ls, actions_ls, rewards, states__ls, dones, avail_acts_oh_ls):
+		dones = tf.cast(dones, dtype="float32")
+		#rewards=rewards
+
+		with tf.GradientTape() as tape:
+			loss = 0
+			for agent_id in range(self.n_agents):
+				states = states_ls[:,agent_id,:]
+				actions = actions_ls[:,agent_id]
+				states_ = states__ls[:,agent_id,:]
+				avail_acts_oh = avail_acts_oh_ls[:,agent_id,:]
+
+				# concatenate one-hot for agent id
+				oh_agentid = tf.one_hot([agent_id]*self.batch_size,self.n_agents)
+
+				agentid_states = tf.concat([oh_agentid,states],1)
+				agentid_states_ = tf.concat([oh_agentid,states_],1)
+				
+				# for ranking the actions
+				q_online_next = self.q_online(agentid_states_)
+
+				# get target actions using onehot availability with online values
+				q_next = tf.where(avail_acts_oh, q_online_next,-1e10)
+				target_actions = tf.math.argmax(q_next,axis=1)
+
+				# grab static values for winning actions
+				q_target_next = self.q_target(agentid_states_)
+				q_target = tf.gather(q_target_next,target_actions,batch_dims=1)
+
+				target = rewards + self.gamma * (1 - dones) * q_target
+				# prevents GradientTape from watching these later
+				target = tf.stop_gradient(target)
+
+				q_online = self.q_online(agentid_states)
+				q_taken = tf.gather(q_online,actions,batch_dims=1)
+
+				# mse
+				error = (target-q_taken)**2
+				agent_loss = tf.reduce_mean(error)
+				loss += agent_loss
+
+		# sonnet
+		variables = self.q_online.trainable_variables
+		gradients = tape.gradient(loss, variables)
+		self.optimizer.apply(gradients, variables)
+
+		return {"loss": loss, "mean q": tf.reduce_mean(q_taken)}
+			
